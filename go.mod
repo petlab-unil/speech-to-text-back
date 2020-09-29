68fbbe9d96bf234e014f0f7838725dfaa35a07a5
sakex <alexandre@senges.ch>
2020-09-30 01:07:16 +0200

    long running

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module speech-to-text-back
 go 1.15
 
 require (
-	cloud.google.com/go v0.64.0
+	cloud.google.com/go v0.66.0
+	cloud.google.com/go/storage v1.12.0 // indirect
 	github.com/gorilla/websocket v1.4.2
 	go.mongodb.org/mongo-driver v1.4.1
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
-	google.golang.org/genproto v0.0.0-20200815001618-f69a88009b70
+	google.golang.org/genproto v0.0.0-20200921151605-7abf4a1a14d5
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
--- src/main.go
+++ src/main.go
@@ -1,16 +1,98 @@
 package main
 
 import (
+	speech "cloud.google.com/go/speech/apiv1"
+	"cloud.google.com/go/storage"
+	"context"
 	"fmt"
-	"net/http"
-	"speech-to-text-back/src/server"
+	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+	"io"
+	"log"
+	"os"
+	"time"
 )
 
-func main() {
-	server.Upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+// uploadFile uploads an object.
+func uploadFile(bucket, object string) error {
+	// bucket := "bucket-name"
+	// object := "object-name"
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	// Open local file.
+	f, err := os.Open("victorien.flac")
+	if err != nil {
+		return fmt.Errorf("os.Open: %v", err)
+	}
+	defer f.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*600)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	wc := client.Bucket(bucket).Object(object).NewWriter(ctx)
+	if _, err = io.Copy(wc, f); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+	fmt.Printf("Blob %v uploaded.\n", object)
+	return nil
+}
+
+func send(client *speech.Client, gcsURI string) error {
+	ctx := context.Background()
+
+	// Send the contents of the audio file with the encoding and
+	// and sample rate information to be transcripted.
+	req := &speechpb.LongRunningRecognizeRequest{
+		Config: &speechpb.RecognitionConfig{
+			Encoding:        speechpb.RecognitionConfig_FLAC,
+			SampleRateHertz: 44100,
+			LanguageCode:    "fr-FR",
+		},
+		Audio: &speechpb.RecognitionAudio{
+			AudioSource: &speechpb.RecognitionAudio_Uri{Uri: gcsURI},
+		},
 	}
 
+	op, err := client.LongRunningRecognize(ctx, req)
+	if err != nil {
+		return err
+	}
+	resp, err := op.Wait(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Print the results.
+	for _, result := range resp.Results {
+		for _, alt := range result.Alternatives {
+			fmt.Printf("\"%v\" (confidence=%3f)\n", alt.Transcript, alt.Confidence)
+		}
+	}
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	client, err := speech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = send(client, "gs://petlabspeechtool/victorien.flac")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+func main() {
 	handler := server.NewHandler()
 
 	s := http.Server{
@@ -26,3 +108,4 @@ func main() {
 		panic("Something went wrong with the server")
 	}
 }
+*/
